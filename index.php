9e9748e2339e11990a4399381bcde39cbcf8317b
Agus Prasetyo <agusprasetyo811@gmail.com>
2013-01-30 08:01:18 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>{TITLE}</title>
+	<link href="{STYLE}style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+	<div id="layout">
+	    <div id="body">
+	    	<div id="header">
+	    		<div align="center" valign="middle"> == HEADER == </div>
+	    	</div>
+	        <div id="menus"></div>
+	        <div id="content">
+	            <table border="0">
+	            	<tr valign="top">
+	                	<td>
+	                    	<div id="content-left"> 
+	                    		<div align="center"> == SIDE LEFT == </div>
+	                        </div>
+	                    </td>
+	                    <td>
+	                    	<div id="content-center">
+	                    		{DASBOR}{MOD_DASBOR}
+	                        </div>
+	                    </td>
+	                    <td>
+	                    	<div id="content-right">
+	                    		<div align="center"> == SIDE RIGHT == </div>
+	                        </div>
+	                    </td>
+	                </tr>
+	            </table>
+	        </div>
+	        <div id="footer">&copy; {SINCE} - {VERSION} {AUTHOR} - <a href="{DEVELOPER}">Link</a></div>
+	    </div>
+	</div>
+</body>
+</html>
